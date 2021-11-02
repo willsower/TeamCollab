1a04351a2acaa404049dd5842b87d95b9f9528e0
Taichen <jatr812@gmail.com>
2021-11-01 18:08:07 -0700

    Update theme.js

--- frontend/utils/theme.js
+++ frontend/utils/theme.js
@@ -4,17 +4,24 @@ import { red } from '@material-ui/core/colors';
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    // Light Gray
     primary: {
-      main: '#556cd6',
+      main: '#D2D3D3',
     },
+    // Dark Gray 
     secondary: {
-      main: '#19857b',
+      main: '#2D3032',
+    },
+    // Pop Color
+    tercerary: {
+      main: '#9615DB',
     },
     error: {
       main: red.A400,
     },
+    // Light Gray
     background: {
-      default: '#fff',
+      default: '#D2D3D3',
     },
   },
 });
