91d70f42ba3dd2eaf588f4817ad10d85c8eb6aa6
Taichen <jatr812@gmail.com>
2021-11-10 13:36:57 -0800

    Created navigation bar
    Created simple navbar

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -1,28 +1,33 @@
 import Link from "next/link";
 
+// CSS
+import styles from "../styles/Nav.module.css";
+
 function Nav() {
   return (
-    <nav className = "w-full h-24 bg-red-200 md:flex md:absolute">
-        {/* Logo */}
-      <div>Logo</div>
+    <nav className="w-full h-14">
+      {/* Logo */}
+      <div className = "md:mt-4 md:ml-4 float-left">
+          <div className = "bg-primary h-7 w-7 rounded-md text-white text-center pt-0.5">TC</div>
+      </div>
 
-      <div className = "md:flex md:relative md:right-0">
+      <div className="md:flex md:mt-4 md:absolute md:right-0">
         {/* Regular Links */}
-        <div>
+        <div className = {styles.nav_link}>
           <Link href="/">Home</Link>
         </div>
-        <div>
+        <div className = {styles.nav_link}>
           <Link href="/">Features</Link>
         </div>
-        <div>
+        <div className = {styles.nav_link}>
           <Link href="/">About</Link>
         </div>
 
         {/* Button Links */}
-        <div>
+        <div className = "button-open mr-4">
           <Link href="/">Login</Link>
         </div>
-        <div>
+        <div className = "button-fill mr-4">
           <Link href="/">Sign Up</Link>
         </div>
       </div>
