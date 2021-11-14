d64d99d01bb75bf10b5a0fcd3cc6045a9d25365e
Taichen <jatr812@gmail.com>
2021-11-13 18:29:24 -0800

    Create home.js
    Created home page

---
+++ frontend/pages/home.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+import Nav from "../../components/nav";
+import Footer from "../../components/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Log In</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <main>
+            Signed in
+      </main>
+
+      <Footer />
+    </>
+  );
+}
