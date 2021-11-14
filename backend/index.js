85164da8bfa619fe0927ed29ecee54857591e94d
Taichen <jatr812@gmail.com>
2021-11-13 18:41:47 -0800

    Adding dependencies and adding in code for mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,41 @@
-const express = require('express')
-const next = require('next')
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3000
-const dev = process.env.NODE_DEV !== 'production' //true false
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler() //part of next config
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const connectStore = require('connect-mongo');
 
-const db = mongoose.connect('mongodb://localhost:27017/Photos')
+// configures for .env files
+require('dotenv').config();
 
-nextApp.prepare().then(() => {
-    // express code here
-    const app = express()
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use('/api/photos', require('./routes/index')) 
-    app.get('*', (req,res) => {
-        return handle(req,res) // for all the react stuff
-    })
-    app.listen(PORT, err => {
-        if (err) throw err;
-        console.log(`ready at http://localhost:${PORT}`)
-    })
-})
+// configure express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// hide express middleware from browser
+app.disable('x-powered-by');
+
+app.use(cors());
+// parse send/receive json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// uri from mongoDB goes here
+const uri = process.env.MONGODB_URL;
+
+// connect to database
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established");
+});
+
+// start server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
